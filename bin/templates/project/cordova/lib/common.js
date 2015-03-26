159ec13d08cbe348000204cf20b572af9a569d16
Les Orchard <me@lmorchard.com>
2015-03-26 16:32:02 -0400

    CB-4548 Initial implementation of list-devices and install-device; refactorings for emulator scripts

--- bin/templates/project/cordova/lib/common.js
+++ bin/templates/project/cordova/lib/common.js
@@ -1,4 +1,15 @@
+var path = require('path');
+var fs = require('fs');
+
+var Promise = require('es6-promise').Promise;
+
 var findSimulators = require('node-firefox-find-simulators');
+var findPorts = require('node-firefox-find-ports');
+var connect = require('node-firefox-connect');
+var installApp = require('node-firefox-install-app');
+var findApp = require('node-firefox-find-app');
+var launchApp = require('node-firefox-launch-app');
+var uninstallApp = require('node-firefox-uninstall-app');
 
 exports.collectSimulatorVersions = function() {
     // There can be multiple Firefox installations, each with their own set of
@@ -12,3 +23,38 @@ exports.collectSimulatorVersions = function() {
         return Object.keys(versions).sort();
     });
 };
+
+exports.pushApp = function (client) {
+
+    var appPath = path.join(path.dirname(path.dirname(__dirname)), 'www');
+    var manifest = JSON.parse(fs.readFileSync(appPath + '/manifest.webapp'));
+
+    // TODO: replace most of this with node-firefox-push-app when it exists
+    // https://github.com/mozilla/node-firefox/issues/37
+    return findApp({
+        client: client,
+        manifest: manifest
+    }).then(function(apps) {
+        return Promise.all(apps.map(function (app) {
+          return uninstallApp({
+              client: client,
+              manifestURL: app.manifestURL
+          });
+        }));
+    }).then(function(uninstalled) {
+        return installApp({
+            client: client,
+            appPath: appPath
+        });
+    }).then(function(result) {
+        return findApp({
+            client: client,
+            manifest: manifest
+        });
+    }).then(function(apps) {
+        return launchApp({
+            client: client,
+            manifestURL: apps[0].manifestURL
+        });
+    });
+}
